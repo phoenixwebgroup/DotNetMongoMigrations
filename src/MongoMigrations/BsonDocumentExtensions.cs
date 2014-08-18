4eeaf359ee81948755b8d6f60473f7d71ae2caa0
Peter Rekdal Sunde <peter.sunde@gmail.com>
2014-08-18 10:04:03 +0200

    upgrade mongocsharpdriver to 1.9.2

--- src/MongoMigrations/BsonDocumentExtensions.cs
+++ src/MongoMigrations/BsonDocumentExtensions.cs
@@ -1,40 +1,30 @@
 ﻿namespace MongoMigrations
 {
-	using System;
-	using System.Linq;
-	using MongoDB.Bson;
-	using MongoDB.Bson.Serialization;
+    using System.Linq;
+    using MongoDB.Bson;
 
-	public static class BsonDocumentExtensions
-	{
-		/// <summary>
-		/// 	Rename all instances of a name in a bson document to the new name.
-		/// </summary>
-		public static void ChangeName(this BsonDocument bsonDocument, string originalName, string newName)
-		{
-			var elements = bsonDocument.Elements
-				.Where(e => e.Name == originalName)
-				.ToList();
-			foreach (var element in elements)
-			{
-				bsonDocument.RemoveElement(element);
-				bsonDocument.Add(new BsonElement(newName, element.Value));
-			}
-		}
+    public static class BsonDocumentExtensions
+    {
+        /// <summary>
+        /// 	Rename all instances of a name in a bson document to the new name.
+        /// </summary>
+        public static void ChangeName(this BsonDocument bsonDocument, string originalName, string newName)
+        {
+            var elements = bsonDocument.Elements
+                .Where(e => e.Name == originalName)
+                .ToList();
+            foreach (var element in elements)
+            {
+                bsonDocument.RemoveElement(element);
+                bsonDocument.Add(new BsonElement(newName, element.Value));
+            }
+        }
 
-		public static object TryGetDocumentId(this BsonDocument bsonDocument)
-		{
-			try
-			{
-				object id;
-				Type idNominalType;
-				IIdGenerator idGenerator;
-				return bsonDocument.GetDocumentId(out id, out idNominalType, out idGenerator);
-			}
-			catch (Exception)
-			{
-				return "Cannot find id";
-			}
-		}
-	}
+        public static object TryGetDocumentId(this BsonDocument bsonDocument)
+        {
+            BsonValue id;
+            bsonDocument.TryGetValue("_id", out id);
+            return id ?? "Cannot find id";
+        }
+    }
 }
